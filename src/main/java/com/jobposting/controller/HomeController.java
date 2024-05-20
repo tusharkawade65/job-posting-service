07b94640a5fedfddb24a49f9ed62631cb3e3852c
tushar kawade <tusharkawade65@gmail.com>
2024-05-21 00:59:11 +0530

    cors error 2

--- src/main/java/com/jobposting/controller/HomeController.java
+++ src/main/java/com/jobposting/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class HomeController {
     TestRepo testRepo;
 
